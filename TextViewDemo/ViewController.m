a2145d79a7984be9ef01c2e24ce43fb95c9bc6f1
liangzhen6 <liangz@szmn.com.cn>
2017-03-11 22:27:33 +0800

    20170311

--- TextViewDemo/ViewController.m
+++ TextViewDemo/ViewController.m
@@ -63,12 +63,12 @@
 
 #pragma mark ================textViewdelegate=========================
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
-    if ([text isEqualToString:@"\n"]){ //判断输入的字是否是回车，即按下return
+    if (textView==self.textView && [text isEqualToString:@"\n"]){ //判断输入的字是否是回车，即按下return
         //在这里做你响应return键的代码
 
         
         textView.text = nil;
-        
+        [self textViewDidChange:textView];
         return NO; //这里返回NO，就代表return键值失效，即页面上按下return，不会出现换行，如果为yes，则输入页面会换行
     }
     return YES;
@@ -101,6 +101,7 @@
     //键盘高度
     CGRect keyBoardFrame = [[[aNotification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
      CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
+    //调增backView距离父视图底部的距离
     _backViewBottomHCons.constant = keyBoardFrame.origin.y != screenH?keyBoardFrame.size.height:0;
 
     [UIView animateWithDuration:duration animations:^{
